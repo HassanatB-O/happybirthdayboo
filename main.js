e72c60bf9091296f58404926bb2e7b3bce387dbb
Hassanat Busari <60980025+HassanatB-O@users.noreply.github.com>
2022-08-17 09:40:24 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+
+document.querySelector(".heart").addEventListener('click', () =>{
+    document.getElementById("main").style.display = "block"
+    document.querySelector(".first-page").style.display = "none"
+    console.log(true)
+})
